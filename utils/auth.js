ce76659f32283b21e10c2099788795c7e9bb5a5a
Aditya Jinegar <aditya_jinegar@yahoo.com>
2025-02-16 16:17:51 -0500

    Implemented verification functionality

--- utils/auth.js
+++ utils/auth.js
@@ -8,4 +8,23 @@ function generateToken(userInfo) {
   return jwt.sign(userInfo, process.env.JWT_SECRET, { expiresIn: '1h' });
 }
 
+function verifyToken(username, token) {
+  return jwt.verify(token, process.env.JWT_SECRET, (error, response) => {
+    if (error) {
+      return { verified: false, message: 'Invalid token' };
+    }
+    if (response.username !== username) {
+      return {
+        verified: false,
+        message: 'Invalid user',
+      };
+    }
+    return {
+      verified: true,
+      message: 'Verified',
+    };
+  });
+}
+
 module.exports.generateToken = generateToken;
+module.exports.verifyToken = verifyToken;
